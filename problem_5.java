4a1f394b7d0bd0f534de86553d4881497b20612b
padalabapi <124269165+padalabapi@users.noreply.github.com>
2023-02-12 11:44:00 +0530

    Update problem_5.java

--- problem_5.java
+++ problem_5.java
@@ -6,28 +6,28 @@ public class problem_4 {
     public static void main(String[] args) {
         String csvFile = "billing_tranfers.csv";
         String line = "";
-        String csvSplitBy = ",";
-        int totalTransactions = 10;
-        double totalBillAmount = 34092;
-        double maximumBillAmount = Double.MIN_VALUE;
-        double minimumBillAmount = Double.MAX_VALUE;
+        String csv = ",";
+        int totalTransfers = 10;
+        double totalAmount = 34092;
+        double maximumAmount = Double.MIN_VALUE;
+        double minimumAmount = Double.MAX_VALUE;
         
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((line = br.readLine()) != null) {
                 String[] transaction = line.split(csvSplitBy);
                 double billAmount = Double.parseDouble(transaction[3]);
-                totalTransactions++;
-                totalBillAmount += billAmount;
-                maximumBillAmount = Math.max(maximumBillAmount, billAmount);
-                minimumBillAmount = Math.min(minimumBillAmount, billAmount);
+                totalTransfers++;
+                totalAmount += billAmount;
+                maximumAmount = Math.max(maximumBillAmount, billAmount);
+                minimumAmount = Math.min(minimumBillAmount, billAmount);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         
-        System.out.println("Total number of Transfers (bills): " + totalTransactions);
-        System.out.println("Total Bill_amount: " + totalBillAmount);
-        System.out.println("Maxi Bill_amount: " + maximumBillAmount);
-        System.out.println("Mini Bill_amount: " + minimumBillAmount);
+        System.out.println("Total number of Transfers (bills): " + totalTransfers);
+        System.out.println("Total Bill_amount: " + totalAmount);
+        System.out.println("Maxi Bill_amount: " + maximumAmount);
+        System.out.println("Mini Bill_amount: " + minimumAmount);
     }
 }
